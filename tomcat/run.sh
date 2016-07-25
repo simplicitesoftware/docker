5e00c2d3049792e82a59fe6f9a4bb06addd4754c
David AZOULAY <dazoulay@simplicite.fr>
2016-07-25 16:10:09 +0200

    Added temp dir

--- tomcat/run.sh
+++ tomcat/run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-rm -fr conf/Catalina work/Catalina logs
-mkdir logs
+rm -fr conf/Catalina work/Catalina logs temp
+mkdir logs temp
 cd bin
 chmod +x *.sh
 ./startup.sh
 LOG=../logs/catalina.out
 while [ ! -f $LOG ]
 do
-        echo "Waiting for Tomcat to start..."
-        sleep 1
+	echo "Waiting for Tomcat to start..."
+	sleep 1
 done
 tail -f $LOG
-
