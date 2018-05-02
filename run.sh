44c1d20390ad47f76e680bc404bc2a431a99526d
David AZOULAY <dazoulay@simplicite.fr>
2018-05-02 16:26:39 +0200

    Git mode

--- run.sh
+++ run.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
-[ "$LOCAL_SMTP_SERVER" = "true" ] && postfix start
+
+if [ "$LOCAL_SMTP_SERVER" = "true" ]
+then
+	echo "Starting postfix SMTP server..."
+	postfix start
+	echo "...done"
+fi
+
+TOMCAT_DIR=/usr/local/tomcat
+TEMPLATE_DIR=/usr/local/template
+if [ -d $TEMPLATE_DIR/.git ]
+then
+	echo "Pulling up-to-date version..."
+	cd $TEMPLATE_DIR
+	git pull
+	echo "...done"
+	echo "Upgrading webapp..."
+	SYNC_EXCLUDES=""
+	[ -d $TOMCAT_DIR/webapps/ROOT/META-INF ] && SYNC_EXCLUDES="--exclude='app/META-INF/context.xml' --exclude='app/WEB-INF/web.xml' --exclude='app/WEB-INF/classes/log4j.xml' --exclude='app/WEB-INF/WEB-INF/patches/V*/patches.properties' --exclude='app/WEB-INF/db' --exclude='app/WEB-INF/dbdoc'"
+	rsync -avhrW --no-compress --progress $SYNC_EXCLUDES app/ $TOMCAT_DIR/webapps/ROOT
+	echo "...done"
+fi
+
 cd /usr/local/tomcat
 ./run.sh -t
