93a3c81408d8f2d1dfb41144f99ec3c57ba3c200
David AZOULAY <dazoulay@simplicite.fr>
2018-05-02 17:02:53 +0200

    Fix

--- run.sh
+++ run.sh
@@ -8,22 +8,19 @@ then
 fi
 
 TOMCAT_DIR=/usr/local/tomcat
+[ ! -d $TOMCAT_DIR/webapps ] && mkdir $TOMCAT_DIR/webapps
 TEMPLATE_DIR=/usr/local/template
 if [ -d $TEMPLATE_DIR/.git ]
 then
 	echo "Pulling up-to-date version..."
 	cd $TEMPLATE_DIR
 	git pull
+	cd ..
 	echo "...done"
 	echo "Upgrading webapp..."
 	SYNC_EXCLUDES=""
-	if [ -d $TOMCAT_DIR/webapps/ROOT ]
-	then
-		mkdir $TOMCAT_DIR/webapps
-		mkdir $TOMCAT_DIR/webapps/ROOT
-		SYNC_EXCLUDES="--exclude='app/META-INF/context.xml' --exclude='app/WEB-INF/web.xml' --exclude='app/WEB-INF/classes/log4j.xml' --exclude='app/WEB-INF/WEB-INF/patches/V*/patches.properties' --exclude='app/WEB-INF/db' --exclude='app/WEB-INF/dbdoc'"
-	fi
-	rsync -avhrW --no-compress --progress $SYNC_EXCLUDES app/ $TOMCAT_DIR/webapps/ROOT
+	[ -d $TOMCAT_DIR/webapps/ROOT ] && SYNC_EXCLUDES="--exclude='app/META-INF/context.xml' --exclude='app/WEB-INF/web.xml' --exclude='app/WEB-INF/classes/log4j.xml' --exclude='app/WEB-INF/WEB-INF/patches/V*/patches.properties' --exclude='app/WEB-INF/db' --exclude='app/WEB-INF/dbdoc'"
+	rsync -avhrW --no-compress --progress $SYNC_EXCLUDES $TEMPLATE_DIR/app/ $TOMCAT_DIR/webapps/ROOT
 	RES=$?
 	[ $RES -ne 0 ] && exit $RES
 	echo "...done"
