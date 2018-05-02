a54b002aa2c6c3f8805c8202e34552dad38147cf
David AZOULAY <dazoulay@simplicite.fr>
2018-05-02 17:35:39 +0200

    Fix

--- run.sh
+++ run.sh
@@ -15,12 +15,11 @@ then
 	echo "Pulling up-to-date version..."
 	cd $TEMPLATE_DIR
 	git pull
-	cd ..
 	echo "...done"
 	echo "Upgrading webapp..."
 	SYNC_OPTS=""
-	[ -d $TOMCAT_DIR/webapps/ROOT ] && SYNC_OPTS="--delete --exclude='META-INF/context.xml' --exclude='WEB-INF/web.xml' --exclude='WEB-INF/classes/log4j.xml' --exclude='WEB-INF/WEB-INF/patches/V*/patches.properties' --exclude='WEB-INF/db' --exclude='WEB-INF/dbdoc'"
-	rsync -avhrW --no-compress $SYNC_OPTS $TEMPLATE_DIR/app/ $TOMCAT_DIR/webapps/ROOT
+	[ -d $TOMCAT_DIR/webapps/ROOT ] && SYNC_OPTS="--delete --exclude='META-INF/context.xml' --exclude='WEB-INF/web.xml' --exclude='WEB-INF/classes/log4j.xml' --exclude='WEB-INF/db/' --exclude='WEB-INF/dbdoc/'"
+	rsync -arW --no-compress $SYNC_OPTS $TEMPLATE_DIR/app/ $TOMCAT_DIR/webapps/ROOT
 	RES=$?
 	[ $RES -ne 0 ] && exit $RES
 	echo "...done"
