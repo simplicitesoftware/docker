d71c4edc6b64781070cb51fe0d8ccfb4f995ff56
Simplicite Software <simplicite@simplicite.io>
2019-05-10 22:29:01 +0200

    Environment variables

--- build-server.sh
+++ build-server.sh
@@ -8,7 +8,8 @@ fi
 
 TAGS="centos alpine"
 [ "$1" != "" ] && TAGS=$1
-SRVS="tomcat tomcat9 tomee"
+#SRVS="tomcat tomcat9 tomee"
+SRVS="tomcat tomcat9"
 [ "$2" != "" ] && SRVS=$2
 
 SERVER=simplicite/server
