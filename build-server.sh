24000af8f353bef94b01691f50608cddc277c6f7
Simplicite Software <simplicite@simplicite.io>
2020-12-21 22:35:56 +0100

    Images meta data

--- build-server.sh
+++ build-server.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "--help" ]
 then
-	echo "Usage: `basename $0` [\"<variants(s), defaults to all>\" [\"<server(s)>, defaults to tomcat]\" [<JVM version, default to latest>]]" >&2
+	echo "Usage: `basename $0` [\"<variants(s), defaults to all>\" [\"<server(s)>, defaults to tomcat]\"]" >&2
 	exit 1
 fi
 
@@ -27,10 +27,6 @@ SRVS=tomcat
 [ "$2" != "" ] && SRVS=$2
 echo "Server(s) = $SRVS"
 
-JVM=latest
-[ "$3" != "" ] && JVM=$3
-echo "JVM = $JVM"
-
 echo "--------------------------------------------------------"
 echo ""
 
@@ -52,23 +48,32 @@ do
 	rm -f tomcat/*.bat tomcat/bin/*.bat tomcat/bin/*.exe tomcat/bin/*.dll
 	echo "Done"
 
-	TAGEXT=""
-	[ $SRV != "tomcat" ] && TAGEXT="-$SRV"
-	[ $JVM != "latest" ] && TAGEXT="$TAGEXT-openjdk-$JVM"
+	SRVEXT=""
+	[ $SRV != "tomcat" ] && SRVEXT="-$SRV"
+
 	for TAG in $TAGS
 	do
-		echo "========================================================"
-		echo "Building $SERVER:$TAG$TAGEXT image..."
-		echo "========================================================"
-		DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
-		FROM=`grep '^FROM' Dockerfile-$TAG | awk '{ print $2 }'`
-		sudo docker pull $FROM
-		sudo docker build --network host -f Dockerfile-$TAG -t $SERVER:$TAG$TAGEXT --build-arg date=$DATE --build-arg jvm=${JVM} .
-		if [ $TAG = "centos-openjdk" -a $SRV = "tomcat" ]
-		then
-			sudo docker build --network host -f Dockerfile-centos-devel -t $SERVER:centos-devel .
-		fi
-		echo "Done"
+		JVMS="latest"
+		[ $TAG = "centos" -o $TAG = "centos8" ] && JVMS="latest 11 1.8.0"
+
+		for JVM in $JVMS
+		do
+			JVMEXT=""
+			[ $JVM != "latest" ] && JVMEXT="-openjdk-$JVM"
+
+			echo "========================================================"
+			echo "Building $SERVER:$TAG$SRVEXT$JVMEXT image..."
+			echo "========================================================"
+			DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
+			FROM=`grep '^FROM' Dockerfile-$TAG | awk '{ print $2 }'`
+			sudo docker pull $FROM
+			sudo docker build --network host -f Dockerfile-$TAG -t $SERVER:$TAG$SRVEXT$JVMEXT --build-arg date=$DATE --build-arg jvm=${JVM} .
+			if [ $TAG = "centos-openjdk" -a $SRV = "tomcat" ]
+			then
+				sudo docker build --network host -f Dockerfile-centos-devel -t $SERVER:centos-devel .
+			fi
+			echo "Done"
+		done
 	done
 done
 rm -fr tomcat
@@ -76,25 +81,34 @@ rm -fr tomcat
 echo ""
 for SRV in $SRVS
 do
-	TAGEXT=""
-	[ $SRV != "tomcat" ] && TAGEXT="-$SRV"
-	[ $JVM != "latest" ] && TAGEXT="$TAGEXT-openjdk-$JVM"
+	SRVEXT=""
+	[ $SRV != "tomcat" ] && SRVEXT="-$SRV"
+
 	for TAG in $TAGS
 	do
-		echo "-- $SERVER:$TAG$TAGEXT ------------------"
-		echo "sudo docker run -it --rm -p 9090:8080 -p 9443:8443 $SERVER:$TAG$TAGEXT"
-		echo "sudo docker push $SERVER:$TAG$TAGEXT"
-		if [ $TAG = "centos" -a $SRV = "tomcat" ]
-		then
-			echo "sudo docker tag $SERVER:$TAG $SERVER:latest"
-			echo "sudo docker push $SERVER:latest"
-			echo "sudo docker rmi $SERVER:latest"
-		fi
-		if [ $TAG = "centos-openjdk" -a $SRV = "tomcat" ]
-		then
-			echo "sudo docker push $SERVER:centos-devel"
-		fi
-		echo ""
+		JVMS="latest"
+		[ $TAG = "centos" -o $TAG = "centos8" ] && JVMS="latest 11 1.8.0"
+
+		for JVM in $JVMS
+		do
+			JVMEXT=""
+			[ $JVM != "latest" ] && JVMEXT="-openjdk-$JVM"
+
+			echo "-- $SERVER:$TAG$SRVEXT$JVMEXT ------------------"
+			echo "sudo docker run -it --rm -p 9090:8080 -p 9443:8443 $SERVER:$TAG$SRVEXT$JVMEXT"
+			echo "sudo docker push $SERVER:$TAG$SRVEXT$JVMEXT"
+			if [ $TAG = "centos" -a $SRV = "tomcat" -a $JVM = "latest" ]
+			then
+				echo "sudo docker tag $SERVER:$TAG $SERVER:latest"
+				echo "sudo docker push $SERVER:latest"
+				echo "sudo docker rmi $SERVER:latest"
+			fi
+			if [ $TAG = "centos-openjdk" -a $SRV = "tomcat" ]
+			then
+				echo "sudo docker push $SERVER:centos-devel"
+			fi
+			echo ""
+		done
 	done
 done
 
