affbdca7bfcdacea3fc39e9b33e228abeac01479
Simplicite Software <simplicite@simplicite.io>
2021-01-17 23:07:07 +0100

    Env vars for HSQLDB

---
+++ hsqldb/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NAME=${DB_NAME:-db};
+USER=${DB_USER:-sa}
+
+echo "================================="
+echo "Java:"
+`java -version`
+echo "Database name: $NAME"
+echo "Database user: $USER"
+echo "================================="
+
+java -cp /usr/local/hsqldb/hsqldb.jar org.hsqldb.server.Server --database.0 "file:/var/lib/hsqldb/${NAME};user=${USER};password=${DB_PASSWORD}" --dbname.0 ${NAME}
