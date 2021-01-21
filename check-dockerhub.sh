1c9a8f9d32df8ae7f16d1f39b38a746b3ddfc94d
Simplicite Software <simplicite@simplicite.io>
2021-01-21 23:19:42 +0100

    Reverse order

--- check-dockerhub.sh
+++ check-dockerhub.sh
@@ -9,11 +9,11 @@ fi
 
 echo ""
 ORG=simplicite
-for REP in `curl -s -H "Authorization: JWT $TOKEN" https://hub.docker.com/v2/repositories/$ORG/ | jq -r '.results[].name' | sort`
+for REP in `curl -s -H "Authorization: JWT $TOKEN" https://hub.docker.com/v2/repositories/$ORG/?page_size=20 | jq -r '.results[].name' | sort`
 do
 	echo "========================================================"
 	printf "== \033[1m%-50s\033[0m ==\n" $ORG/$REP
 	echo "========================================================"
-	curl -s -H "Authorization: JWT $TOKEN" https://hub.docker.com/v2/repositories/$ORG/$REP/tags/ | jq -r '.results[] | "\(.last_updated) \(.name) \(.images[].digest)"' | sort | sed 's/T/ /;s/\.[0-9]*Z//;s/sha256://' | awk '{ printf "\033[32m%-10s %-8s\033[0m %s \033[1m\033[34m%-20s\033[0m\n", $1, $2, substr($4,0,16), $3 }' || exit 2
+	curl -s -H "Authorization: JWT $TOKEN" https://hub.docker.com/v2/repositories/$ORG/$REP/tags/?page_size=50 | jq -r '.results[] | "\(.last_updated) \(.name) \(.images[].digest)"' | sort -r | sed 's/T/ /;s/\.[0-9]*Z//;s/sha256://' | awk '{ printf "\033[32m%-10s %-8s\033[0m %s \033[1m\033[34m%-20s\033[0m\n", $1, $2, substr($4,0,16), $3 }' || exit 2
 	echo ""
 done
