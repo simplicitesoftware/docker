ea9573bb3277fd537e78c6436903c211b2fb574b
David AZOULAY <dazoulay@simplicite.fr>
2021-01-22 13:58:03 +0100

    Styles

--- check-dockerhub.sh
+++ check-dockerhub.sh
@@ -7,13 +7,16 @@ then
 	exit 1
 fi
 
-echo ""
 ORG=simplicite
+[ -x /usr/bin/figlet ] && echo "" && /usr/bin/figlet -f small ${ORG^}
+echo ""
 for REP in `curl -s -H "Authorization: JWT $TOKEN" https://hub.docker.com/v2/repositories/$ORG/?page_size=20 | jq -r '.results[].name' | sort`
 do
 	echo "========================================================"
 	printf "== \033[1m%-50s\033[0m ==\n" $ORG/$REP
 	echo "========================================================"
-	curl -s -H "Authorization: JWT $TOKEN" https://hub.docker.com/v2/repositories/$ORG/$REP/tags/?page_size=50 | jq -r '.results[] | "\(.last_updated) \(.name) \(.images[].digest)"' | sort -r | sed 's/T/ /;s/\.[0-9]*Z//;s/sha256://' | awk '{ printf "\033[32m%-10s %-8s\033[0m %s \033[1m\033[34m%-20s\033[0m\n", $1, $2, substr($4,0,16), $3 }' || exit 2
+	curl -s -H "Authorization: JWT $TOKEN" https://hub.docker.com/v2/repositories/$ORG/$REP/tags/?page_size=50 | jq -r '.results[] | "\(.last_updated) \(.name) \(.images[].digest)"' | sort -r | sed 's/T/ /;s/\.[0-9]*Z//;s/sha256://' | awk '{ printf "\033[34;1m%-10s %-8s\033[0m %s \033[31;1m%-20s\033[0m\n", $1, $2, substr($4,0,16), $3 }' || exit 2
 	echo ""
 done
+
+exit 0
