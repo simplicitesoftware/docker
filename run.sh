ec87da64120ca081b7c932565978c39f1f98c92e
Simplicite Software <simplicite@simplicite.io>
2021-02-09 18:46:02 +0100

    Force cp for .ssh

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@ fi
 TOMCAT_DIR=/usr/local/tomcat
 [ ! -d $TOMCAT_DIR/webapps ] && mkdir $TOMCAT_DIR/webapps
 
-[ -d $TOMCAT_DIR/.ssh ] && cp -r $TOMCAT_DIR/.ssh /root
+[ -d $TOMCAT_DIR/.ssh ] && cp -fr $TOMCAT_DIR/.ssh /root
 if [ ! -z "$SSH_KNOWN_HOSTS" ]
 then
 	[ ! -d /root/.ssh ] && mkdir /root/.ssh
