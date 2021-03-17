4dc900e6f69b2ffb3c6be47e1c321dbc7f9bde43
David AZOULAY <dazoulay@simplicite.fr>
2021-03-17 18:55:50 +0100

    Oracle & SQL server example

---
+++ oraclexe/init.sql
@@ -0,0 +1,7 @@
+create user simplicite identified by simplicite;
+grant connect, resource to simplicite;
+grant create sequence to simplicite;
+grant create procedure to simplicite;
+grant exp_full_database, imp_full_database to simplicite;
+alter profile default limit password_life_time unlimited;
+alter user simplicite identified by simplicite account unlock;
