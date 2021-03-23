09d2ce444a74e7ebbae76df97fc33365239a7e28
David AZOULAY <dazoulay@simplicite.fr>
2021-03-23 10:04:34 +0100

    Unlimited tablespace for Oracle 18c

--- examples/databases/oraclexe/init.sql
+++ examples/databases/oraclexe/init.sql
@@ -3,6 +3,7 @@ alter session set "_ORACLE_SCRIPT"=true;
 
 create user simplicite identified by simplicite;
 grant connect, resource to simplicite;
+grant unlimited tablespace to simplicite;
 grant create sequence to simplicite;
 grant create procedure to simplicite;
 grant exp_full_database, imp_full_database to simplicite;
