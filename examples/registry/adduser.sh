4f2c432f96429f558743836832399ccb1392aa81
David AZOULAY <dazoulay@simplicite.fr>
2021-04-06 18:58:06 +0200

    Added registry example

---
+++ examples/registry/adduser.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$1" = "" -o "$1" = "--help" ]
+then
+	echo "Usage: `basename $0` <user>" >&2
+	exit -1
+fi
+
+pushd `dirname $0` > /dev/null
+DIR=`pwd`
+popd > /dev/null
+
+USER=$1
+PWD=$2
+[ "$PWD" = "" ] && PWD=`date | md5sum | awk '{print $1}'`
+
+echo "User: $USER"
+echo "Password: $PWD"
+[ ! -d $DIR/auth ] && mkdir $DIR/auth
+[ ! -f $DIR/auth/users.pwd ] && touch $DIR/auth/users.pwd
+htpasswd -B -b $DIR/auth/users.pwd $USER $PWD
+exit $?
