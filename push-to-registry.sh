74161b5482c70eefda1843f1121c75d79a51073a
Simplicite Software <simplicite@simplicite.fr>
2021-04-09 22:51:48 +0000

    Flexible push

--- push-to-registry.sh
+++ push-to-registry.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
-IMGS=${1:-server platform theia}
+if [ "$1" = "" -o "$1" = "--help" ]
+then
+	echo "Usage: `basename $0` <server|platform|theia> [<tags>]" >&2
+	exit -1
+fi
 
-for IMG in $IMGS
+IMG=$1
+
+[ -x /usr/bin/figlet ] && echo "" && /usr/bin/figlet -f small ${IMG^}
+
+TAGS=$2
+[ "$TAGS" = "" ] && TAGS=`docker images | grep "^simplicite.$IMG" | awk '{print $2}'`
+
+for TAG in $TAGS
 do
 	echo ""
 	echo "------------------------------------"
-	echo "$IMG"
+	echo "Image $IMG:$TAG"
 	echo "------------------------------------"
+	echo ""
+	echo "Pushing image $IMG:$TAG to DockerHub registry"
+	docker push simplicite/$IMG:$TAG
+	echo "Done"
 
-	for TAG in `docker images | grep "^simplicite.$IMG" | awk '{print ":"$2}'`
-	do
-		echo ""
-		echo "Pushing image $IMG$TAG to DockerHub registry"
-		docker push simplicite/$IMG$TAG
+	if [ "$DOCKER_PRIVATE_REGISTRY_HOST" != "" ]
+	then
+		echo "Pushing image $IMG:$TAG to local registry $DOCKER_PRIVATE_REGISTRY_HOST"
+		docker tag simplicite/$IMG:$TAG $DOCKER_PRIVATE_REGISTRY_HOST/$IMG:$TAG
+		docker push $DOCKER_PRIVATE_REGISTRY_HOST/$IMG:$TAG
+		docker rmi $DOCKER_PRIVATE_REGISTRY_HOST/$IMG:$TAG
 		echo "Done"
-
-		if [ "$DOCKER_PRIVATE_REGISTRY_HOST" != "" ]
-		then
-			echo "Pushing image $IMG$TAG to local registry $DOCKER_PRIVATE_REGISTRY_HOST"
-			docker tag simplicite/$IMG$TAG $DOCKER_PRIVATE_REGISTRY_HOST/$IMG$TAG
-			docker push $DOCKER_PRIVATE_REGISTRY_HOST/$IMG$TAG
-			docker rmi $DOCKER_PRIVATE_REGISTRY_HOST/$IMG$TAG
-			echo "Done"
-		fi
-	done
+	fi
 done
 echo ""
+
 exit 0
 
