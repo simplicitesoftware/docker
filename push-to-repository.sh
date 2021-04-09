2a9a4a505de188d2650aae960beeb5ca23c316cc
Simplicite Software <simplicite@simplicite.io>
2021-04-09 17:33:10 +0200

    fix

--- push-to-repository.sh
+++ push-to-repository.sh
@@ -12,10 +12,10 @@ do
         for TAG in `docker images | grep "^simplicite.$IMG" | awk '{print ":"$2}'`
         do
                 echo ""
-                echo "Pushing image: $IMG$TAG to DockerHub registry"
+                echo "Pushing image $IMG$TAG to DockerHub registry"
                 echo sudo docker push simplicite/$IMG$TAG
                 echo "Done"
-                echo "Pushing image: $IMG$TAG to local registry"
+                echo "Pushing image $IMG$TAG to local registry"
                 echo sudo docker tag simplicite/$IMG$TAG localhost:5000/$IMG$TAG;
                 echo sudo docker push localhost:5000/$IMG$TAG
                 echo sudo docker rmi localhost:5000/$IMG$TAG
