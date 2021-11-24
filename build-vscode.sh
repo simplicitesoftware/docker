e22da5df1be319ded1d06389a7f81cd97aef4368
David AZOULAY <dazoulay@simplicite.fr>
2021-11-24 12:37:04 +0100

    Fix

--- build-vscode.sh
+++ build-vscode.sh
@@ -19,7 +19,7 @@ BASE="simplicite/ide-base:latest"
 docker inspect $BASE > /dev/null 2>&1
 if [ $? -ne 0 ]
 then
-	echo "-- $IMG ------------------"
+	echo "-- $BASE ------------------"
 	docker build --network host -t $BASE -f Dockerfile-ide-base .
 	echo ""
 fi
