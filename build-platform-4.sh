15fa5e25faf696a10e34d5d393801a89a6021999
Simplicite Software <simplicite@simplicite.fr>
2022-01-21 19:04:39 +0100

    Indent

--- build-platform-4.sh
+++ build-platform-4.sh
@@ -11,9 +11,11 @@ docker rmi simplicite/platform:4.0
 docker tag simplicite/platform:4.0-latest simplicite/platform:4.0
 
 ./push-to-registries.sh platform \
-	4.0-latest-temurin-11 4.0-latest-temurin-17 \
+	4.0-latest-temurin-11 \
+	4.0-latest-temurin-17 \
 	4.0-latest-openjdk-11 \
-	4.0-latest 4.0
+	4.0-latest \
+	4.0
 
 ./build-platform.sh --delete 4.0-latest-light
 
@@ -26,8 +28,11 @@ docker rmi simplicite/platform:4.0-light
 docker tag simplicite/platform:4.0-latest-light simplicite/platform:4.0-light
 
 ./push-to-registries.sh platform \
-	4.0-latest-light-temurin-11 4.0-latest-light-temurin-17 \
-	4.0-latest-light-openjdk-1.8.0 4.0-latest-light-openjdk-11 \
-	4.0-latest-light 4.0-light
+	4.0-latest-light-temurin-11 \
+	4.0-latest-light-temurin-17 \
+	4.0-latest-light-openjdk-1.8.0 \
+	4.0-latest-light-openjdk-11 \
+	4.0-latest-light \
+	4.0-light
 
 exit 0
