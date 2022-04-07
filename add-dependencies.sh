3fd8a6ff2e1cf9954f481b923cd344a84b732d7b
Simplicite Software <simplicite@simplicite.fr>
2022-04-07 14:17:44 +0000

    Milestone

--- add-dependencies.sh
+++ add-dependencies.sh
@@ -6,7 +6,7 @@ then
 	exit -1
 fi
 
-if [ "$1" = "" -o "$1" = "--help"]
+if [ "$1" = "" -o "$1" = "--help" ]
 then
 	echo "Usage `basename $0` <list of groupId:artifactId:version>" >&2
 	exit 1
@@ -19,9 +19,13 @@ then
 	shift
 fi
 
-DIR="/tmp/`basename $0 .sh`-$$"
-mkdir $DIR
-pushd $DIR
+REG="$HOME/.m2"
+DIR=`dirname $0`
+TMP="$DIR/`basename $0 .sh`-$$"
+trap "rm -fr $REG $TMP" EXIT
+
+mkdir $TMP
+pushd $TMP
 
 cat << EOF > pom.xml
 <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
@@ -32,13 +36,14 @@ cat << EOF > pom.xml
   <name>Temporary</name>
 EOF
 
+N=0
 for DEP in $*
 do
 	GROUP=`echo $DEP | cut -d : -f 1`
 	ARTIFACT=`echo $DEP | cut -d : -f 2`
 	VERSION=`echo $DEP | cut -d : -f 3`
 
-	if [ !-z "$GROUP" -a !-z "$ARTIFACT" -a !-z "$VERSION" ]
+	if [ ! -z "$GROUP" -a ! -z "$ARTIFACT" -a ! -z "$VERSION" ]
 	then
 		cat << EOF >> pom.xml
   <dependencies>
@@ -50,8 +55,9 @@ do
   </dependencies>
 EOF
 		echo "Added dependency: $DEP"
+		N=`expr $N + 1`
 	else
-		echo "Ignored malformed dependency: $DEP"
+		echo "Ignored malformed dependency: $DEP" >&2
 	fi
 done
 
@@ -59,11 +65,19 @@ cat << EOF >> pom.xml
 </project>
 EOF
 
+if [ $N -eq 0 ]
+then
+	echo "No dependency to add: $DEP" >&2
+	exit 3
+fi
+
 mvn -U dependency:copy-dependencies
 
 ls -alF target/lib
+# TODO
+# - check each JAR vs instance JARs
+# - copy each JAR (takin FORCE into acount)
 
 popd
-rm -fr $DIR
 
 exit 0
