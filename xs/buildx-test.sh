89dff86a3e0c2a8e3fbdbe5bd33bb5d6fb518d73
Simon Campano <scampano@simplicite.fr>
2022-06-01 12:36:25 +0200

    update xs

--- xs/buildx-test.sh
+++ xs/buildx-test.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-for SIMPLICITE_BASE_TAG in 5-beta 5-beta-light 5-latest 5-latest-light
+for SIMPLICITE_BASE_TAG in 5-beta
 do
     # Base image from which the dockerfile copies the webapp
     SIMPLICITE_BASE="simplicite/platform:$SIMPLICITE_BASE_TAG"
     
     # Name of the built image
-    TARGET_TAG="$SIMPLICITE_BASE_TAG-xs"
+    TARGET_TAG="$SIMPLICITE_BASE_TAG-xs-test-bdx"
     TARGET_IMAGE="simplicite/platform:$TARGET_TAG"
 
     # Run build (see `./Dockerfile``)
-    docker build --pull \
+    docker buildx build --pull \
+        --platform linux/arm/v7,linux/arm64,linux/amd64 \
         --build-arg SIMPLICITE_BASE=$SIMPLICITE_BASE \
         -t $TARGET_IMAGE .
 
@@ -22,4 +23,5 @@ do
 done
 
 docker image prune -f
+# ./delete-from-private-registry.sh platform 5-beta-xs-test-bdx
 # docker run -it --rm --name sim_arm -p 8080:8080 $TARGET_IMAGE 
