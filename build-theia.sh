4389235c705fea73c89fbc68f1915aac5caa2a42
David AZOULAY <dazoulay@simplicite.fr>
2022-06-07 14:49:50 +0200

    Exit fct

--- build-theia.sh
+++ build-theia.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-if [ "$1" = "--help" ]
-then
-	echo "Usage: `basename $0`" >&2
-	exit 1
-fi
+exit_with () {
+	[ "$2" != "" ] && echo $2 >&2
+	exit ${2:-1}
+}
+
+[ "$1" = "--help" ] && exit_with 1 "Usage: `basename $0`"
 
 LOCK=/tmp/`basename $0 .sh`.lck
 trap "rm -f $LOCK" TERM INT QUIT HUP
-if [ -f $LOCK ]
-then
-	echo "A build process is in process since `cat $LOCK`" >&2
-	exit 2
-fi
+[ -f $LOCK ] && exit_with 2 "A build process is in process since `cat $LOCK`"
 date > $LOCK
 
 cd theia
@@ -24,7 +21,7 @@ echo "========================================================"
 DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 FROM=`grep '^FROM' Dockerfile | awk '{ print $2 }'`
 docker pull $FROM
-docker build --network host -t $IMG --build-arg THEIA_TAG=$TAG --build-arg BUILD_DATE=$DATE .
+docker build --network host -t $IMG --build-arg THEIA_TAG=$TAG --build-arg BUILD_DATE=$DATE .  || exit_with 3 "Unable to build image $IMG"
 echo "Done"
 echo ""
 echo "docker run -it --rm -p 127.0.0.1:3030:3030 --name=theia $IMG"
--- build-vscode.sh
+++ build-vscode.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-if [ "$1" = "--help" ]
-then
-	echo "Usage: `basename $0`" >&2
-	exit 1
-fi
+exit_with () {
+	[ "$2" != "" ] && echo $2 >&2
+	exit ${2:-1}
+}
+
+[ "$1" = "--help" ] && exit_with 1 "Usage: `basename $0`"
 
 LOCK=/tmp/`basename $0 .sh`.lck
 trap "rm -f $LOCK" TERM INT QUIT HUP
-if [ -f $LOCK ]
-then
-	echo "A build process is in process since `cat $LOCK`" >&2
-	exit 2
-fi
+[ -f $LOCK ] && exit_with 2 "A build process is in process since `cat $LOCK`"
 date > $LOCK
 
 cd vscode
@@ -22,7 +19,7 @@ echo "========================================================"
 echo "Building $IMG image..."
 echo "========================================================"
 DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
-docker build --network host -t $IMG --build-arg BUILD_DATE=$DATE .
+docker build --network host -t $IMG --build-arg BUILD_DATE=$DATE . || exit_with 3 "Unable to build image $IMG"
 echo "Done"
 echo ""
 echo "docker run -it --rm -p 127.0.0.1:3030:3030 --name=vscode $IMG"
