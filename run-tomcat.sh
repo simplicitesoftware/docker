487bee5a4d22ec85f639d6524335976e5391c7b1
David AZOULAY <dazoulay@simplicite.fr>
2023-01-24 16:20:58 +0100

    Comments

--- run-tomcat.sh
+++ run-tomcat.sh
@@ -7,8 +7,12 @@ echo " |___/_|_|_|_| .__/_|_\\__|_|\\__\\___(_)_\\___/"
 echo "             |_| Running on $DOCKER"
 echo ""
 
+# Start a local SMTP server for development
 if [ "$LOCAL_SMTP_SERVER" = "true" ]
 then
+	echo "------------------------------------------------------------------------"
+	echo "WARNING: Starting a local SMTP server is ABSOLUTELY not suitable for production"
+	echo "------------------------------------------------------------------------"
 	echo "Starting SMTP server..."
 	postfix start
 	echo "...done"
@@ -26,7 +30,7 @@ TOMCAT_GID=$(id -g $TOMCAT_USER)
 if [ $TOMCAT_USER = "root" ]
 then
 	echo "------------------------------------------------------------------------"
-	echo "WARNING: Running Tomcat as root, this may not be suitable for production"
+	echo "WARNING: Running Tomcat as root, this MAY not be suitable for production"
 	echo "------------------------------------------------------------------------"
 else
 	echo "Running Tomcat as $TOMCAT_USER (user ID $TOMCAT_UID, group ID $TOMCAT_GID)"
