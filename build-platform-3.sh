d33ad3ae41fa9ab4cac295545432940ebea2eb7f
David AZOULAY <dazoulay@simplicite.fr>
2023-08-01 16:29:57 +0200

    Fix

--- build-platform-3.sh
+++ build-platform-3.sh
@@ -13,9 +13,9 @@ VERSIONS="3.0 3.1 3.2"
 for VERSION in $VERSIONS
 do
 	./build-platform.sh --delete $VERSION || exit_with $? "Unable to build platform version $VERSION"
-	docker rmi $REGISTRY/platform:$VERSION
-	docker tag $REGISTRY/platform:$VERSION-openjdk-1.8.0 $REGISTRY/platform:$VERSION
-	docker rmi $REGISTRY/platform:$VERSION-openjdk-1.8.0
+	docker rmi $REGISTRY/platform:$VERSION > /dev/null 2>&1
+	docker tag $REGISTRY/platform:$VERSION-adoptium-8 $REGISTRY/platform:$VERSION
+	docker rmi $REGISTRY/platform:$VERSION-adoptium-8
 done
 
 ./push-to-registries.sh --delete platform $VERSIONS
