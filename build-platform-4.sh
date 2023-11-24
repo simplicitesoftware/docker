6b37c112ba5bf4ddf42e1fc4cfaf12b0ae904528
Simplicite Software <simplicite@simplicite.fr>
2023-11-24 11:49:46 +0100

    Fixes

--- build-platform-4.sh
+++ build-platform-4.sh
@@ -27,10 +27,8 @@ docker tag $REGISTRY/platform:4.0-latest $REGISTRY/platform:4.0
 
 if [ $PUSH -eq 1 ]
 then
-	./push-to-registries.sh --delete platform \
-	./push-to-registries.sh platform \
-		4.0-latest \
-		4.0
+	./push-to-registries.sh --delete platform 4.0
+	./push-to-registries.sh platform 4.0-latest
 fi
 
 ./build-platform.sh --delete 4.0-light || exit_with $? "Unable to build platform version 4.0-latest-light"
@@ -44,9 +42,8 @@ docker tag $REGISTRY/platform:4.0-latest-light $REGISTRY/platform:4.0-light
 
 if [ $PUSH -eq 1 ]
 then
-	./push-to-registries.sh --delete platform \
-		4.0-latest-light \
-		4.0-light
+	./push-to-registries.sh --delete platform 4.0-light
+	./push-to-registries.sh platform 4.0-latest-light
 fi
 
 exit_with
