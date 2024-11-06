c15eb8ecb14ac3471bd9c88172e15944fb94b1da
Simplicite Software <simplicite@simplicite.fr>
2024-11-06 12:53:11 +0100

    Aliases for 4.0

--- build-platform-4.sh
+++ build-platform-4.sh
@@ -25,10 +25,16 @@ docker rmi $REGISTRY/platform:4.0-centos-17
 docker rmi $REGISTRY/platform:4.0 > /dev/null 2>&1
 docker tag $REGISTRY/platform:4.0-latest $REGISTRY/platform:4.0
 
+docker rmi $REGISTRY/platform:4-latest > /dev/null 2>&1
+docker tag $REGISTRY/platform:4.0-latest $REGISTRY/platform:4-latest
+
+docker rmi $REGISTRY/platform:4 > /dev/null 2>&1
+docker tag $REGISTRY/platform:4.0-latest $REGISTRY/platform:4
+
 if [ $PUSH -eq 1 ]
 then
-	./push-to-registries.sh --delete platform 4.0
-	./push-to-registries.sh platform 4.0-latest
+	./push-to-registries.sh --delete platform 4.0 4.0-latest 4
+	./push-to-registries.sh platform 4-latest
 fi
 
 ./build-platform.sh --delete 4.0-light || exit_with $? "Unable to build platform version 4.0-latest-light"
@@ -40,9 +46,15 @@ docker rmi $REGISTRY/platform:4.0-light-centos-17
 docker rmi $REGISTRY/platform:4.0-light > /dev/null 2>&1
 docker tag $REGISTRY/platform:4.0-latest-light $REGISTRY/platform:4.0-light
 
+docker rmi $REGISTRY/platform:4-latest-light > /dev/null 2>&1
+docker tag $REGISTRY/platform:4.0-latest-light $REGISTRY/platform:4-latest-light
+
+docker rmi $REGISTRY/platform:4-light > /dev/null 2>&1
+docker tag $REGISTRY/platform:4.0-latest-light $REGISTRY/platform:4-light
+
 if [ $PUSH -eq 1 ]
-then
-	./push-to-registries.sh --delete platform 4.0-light 4.0-latest-light
+then²
+	./push-to-registries.sh --delete platform 4.0-light 4.0-latest-light 4-latest-light 4-light
 fi
 
 exit_with
