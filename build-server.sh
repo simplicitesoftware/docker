cf09eb681d3e90092b7ad68d44fe5d86136030e4
David AZOULAY <dazoulay@simplicite.fr>
2025-08-05 19:17:03 +0200

    Removed temurin images

--- build-server.sh
+++ build-server.sh
@@ -36,7 +36,7 @@ echo ""
 echo "--------------------------------------------------------"
 
 #TAGS=${1:-centos-base centos centos-jvmless almalinux8-base almalinux8 almalinux8-jvmless almalinux9-base almalinux9 almalinux9-jvmless alpine-base alpine eclipse-temurin devel}
-TAGS=${1:-almalinux9-base almalinux9 almalinux9-jvmless alpine-base alpine eclipse-temurin devel}
+TAGS=${1:-almalinux9-base almalinux9 almalinux9-jvmless alpine-base alpine devel}
 echo "Variants(s) = $TAGS"
 
 # Server
